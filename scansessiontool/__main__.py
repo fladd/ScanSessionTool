5dd31c7bf61c502b2e01626ea9d1843ec794243c
Florian Krause <siebenhundertzehn@gmail.com>
2024-06-11 15:40:18 +0200

    Update __main__.py

--- scansessiontool/__main__.py
+++ scansessiontool/__main__.py
@@ -1,4 +1,3 @@
-import os
 import platform
 from tkinter import *
 from tkinter.ttk import *
@@ -8,13 +7,6 @@ from .scansessiontool import ScanSessionTool
 
 def run():
     root = Tk()
-    if platform.system() == "Windows":
-        root.iconbitmap(os.path.abspath(os.path.join(
-            os.path.split(__file__)[0], "sst_icon.ico")))
-    else:
-        root.tk.call('wm', 'iconphoto', root._w,
-                     PhotoImage(file=os.path.abspath(os.path.join(
-                os.path.split(__file__)[0], "sst_icon.png"))))
     app = ScanSessionTool(root)
     app.mainloop()
 
